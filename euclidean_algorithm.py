600b35a4a010436333847acf067f7692201d01e1
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-09-13 17:58:12 -0700

    added comments

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -3,7 +3,13 @@ This program calculates the greatest common divsor
 of two numbers
 @author Sergio, Vasquez
 @version 08/13/2020
-@file euclidean_algorithm.py'''
+@file euclidean_algorithm.py
+'''
 def gcd(a, b):
+    '''
+    a: a non-negative integer
+    b: a non-negative integer less than or equal to a
+    return: a integer that is the greatest common divisor of a and b
+    '''
     return a if b == 0 else gcd(b, a % b)
 
