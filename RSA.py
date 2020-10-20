047c98fb9df675a403aa235d37de22da8f9e5175
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-10-19 21:23:21 -0700

    added RSA

--- RSA.py
+++ RSA.py
@@ -10,15 +10,15 @@ def gcd(a, b):
     a: a non-negative integer
     b: a non-negative integer less than or equal to a
     returns an integer that is the greater common divisor of a and b'''
-    return a if b == 0 else gcd(b, b % a)
+    return a if b == 0 else gcd(b, a % b)
 
 def relative_prime_number(n):
     '''
     n: a positive integer
     returns an integer that is relatively prime to this number
     return None if such a number does not exist'''
-    for i in range(1, n):
-        if gcd(i, n) == 1:
+    for i in range(2, n):
+        if gcd(n, i) == 1:
             return i
     return None
 
