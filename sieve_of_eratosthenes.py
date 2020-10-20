9b3ee3d4344167b68d3af6fd1f082836719d5bc0
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-10-19 19:58:19 -0700

    added comments

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,6 +1,19 @@
+'''
+This program uses the sieve of Eratosthenes
+to find all primes less than 10,000
+@author Sergio Vasquez
+@version 10/19/2020
+@file sieve_of_eratosthenes.py
+'''
 from math import *
 
 def sieve(n):
+    '''
+    n: An integer that is greater than or equal
+    to 2
+    return a list of all primes that are 
+less than or equal to n
+    '''
     marked = [True for i in range(n + 1)]
 
     for i in range(2, int(sqrt(n)) + 1):
